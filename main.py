6da76c8e365aa1e620911e4c50bdb79f5f528eac
ISBaloch <isbaloch860@gmail.com>
2020-03-26 20:47:28 +0500

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask,  render_template, request
+app  = Flask(__name__)
+import pickle
+
+
+
+file = open('model.pkl', 'rb')
+clf = pickle.load(file)
+file.close()
+@app.route('/', methods=["GET", "POST"])
+def hello_world():
+    if request.method == "POST":
+        myDict = request.form
+        fever = int(myDict['fever'])
+        Age = int(myDict['age'])
+        Pain = int(myDict['Pain'])
+        RunnyNose = int(myDict['Runny Nose'])
+        DiffBreath = int(myDict['Difficulty Breathing'])
+
+    # Code for inference
+        infprob = clf.predict_proba([[fever, Pain, Age, RunnyNose, DiffBreath]])[0][1]
+        infprob
+        print(infprob)
+        return render_template('show.html', inf=round(infprob*100))
+    return render_template('index.html')
+    # return 'Hello, World!' +str(infprob)
+
+
+if __name__ == "__main__":
+     app.run(debug=True)
